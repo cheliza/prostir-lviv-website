305ae125ec754c559e6c9d866395a5d3790cc6ff
cheliz <roikeliz2006@gmail.com>
2025-10-20 23:43:07 +0300

    add migration for movies, menu

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Event, Info
+from .models import Event, Info, Movie, MenuItem
 
 admin.site.register(Event)
 admin.site.register(Info)
+admin.site.register(Movie)
+admin.site.register(MenuItem)
--- backend/core/models.py
+++ backend/core/models.py
@@ -34,3 +34,44 @@ class Info(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Назва фільму")
+    date = models.DateField(verbose_name="Дата показу")
+    time = models.TimeField(blank=True, null=True, verbose_name="Час початку показу")
+    description = models.TextField(verbose_name="Опис", blank=True, null=True)
+    image = models.ImageField(upload_to='movies/', verbose_name="Постер", blank=True, null=True)
+    location = models.CharField(max_length=255, verbose_name="Локація", blank=True, null=True)
+    is_visible = models.BooleanField(default=True, verbose_name="Показувати на сайті")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фільм"
+        verbose_name_plural = "Фільми"
+        ordering = ['-date', '-time']
+
+
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = [
+        ('food', 'Їжа'),
+        ('drink', 'Напій'),
+    ]
+    
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, verbose_name="Категорія")
+    subcategory = models.CharField(max_length=100, verbose_name="Підкатегорія (напр. 'Кава', 'Бургери')", blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name="Назва")
+    description = models.TextField(verbose_name="Опис", blank=True, null=True)
+    weight_grams = models.IntegerField(verbose_name="Вага (грами)", blank=True, null=True)
+    volume_ml = models.IntegerField(verbose_name="Об'єм (мл)", blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+    class Meta:
+        verbose_name = "Позиція меню"
+        verbose_name_plural = "Меню"
+        ordering = ['category', 'name']
